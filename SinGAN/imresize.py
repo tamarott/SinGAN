7b0b418e34a1ad1d71a633e595e747c6d7a01447
tamarott <tamarott@gmail.com>
2020-02-24 15:15:32 +0200

    update credit for imresize.py

--- SinGAN/imresize.py
+++ SinGAN/imresize.py
@@ -1,3 +1,5 @@
+# This code was taken from: https://github.com/assafshocher/resizer by Assaf Shocher
+
 import numpy as np
 from scipy.ndimage import filters, measurements, interpolation
 from skimage import color
