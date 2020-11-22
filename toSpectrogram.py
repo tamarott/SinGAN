e008a9aed5307fb4834a41afca2c455e6efcc1d6
Cecilie Neckelmann <cecilieneckelmann@Cecilies-MacBook-Pro-3.local>
2020-11-22 12:35:35 +0100

    Spectrogram adding files to input folder

--- toSpectrogram.py
+++ toSpectrogram.py
@@ -11,14 +11,28 @@ import IPython.display as ipd
 
 # Finding the desired audio file
 # audio_fpath = "../Input/Sounds/"
-audio_fpath = "/Users/cecilieneckelmann/Documents/SinGAN-1/Input/Sounds/"
+audio_fpath = "/Users/cecilieneckelmann/Documents/ResearchProject/SinGan/Input/Sounds/"
 audio_clips = os.listdir(audio_fpath)
 print("No. of .wav files in audio folder = ",len(audio_clips))
+
+wavDict = {}
+counter = 0
+print('Names')
+for name in audio_clips:
+    shortname = name.split('.')
+    wavDict[counter] = shortname[0]
+    counter += 1
+
+# Picking a file to work with:
+noSound = 0
+nameOfSound = wavDict[noSound]
 
 
 # Returns a time series, sr is sample rate (how many samples per sec). 
 # Explanation: Typically an audio signal, denoted by y, and represented as a one-dimensional numpy.ndarray of floating-point values. y[t] corresponds to the amplitude of the waveform at sample t. 
-x, sr = librosa.load(audio_fpath+audio_clips[0], sr=44100)
+
+
+x, sr = librosa.load(audio_fpath+audio_clips[noSound], sr=44100)
 
 print(type(x), type(sr))
 print(x.shape, sr)
@@ -28,6 +42,10 @@ plt.figure(figsize=(14, 5))
 
 
 librosa.display.waveplot(x, sr=sr)
+
+print("This is x: ")
+print(x)
+print("Length: " + str(len(x)))
 
 # stft(x): Short-Time Fourier Transform
 # This function returns a complex-valued matrix D such that 
@@ -48,9 +66,11 @@ Xdb = librosa.amplitude_to_db(abs(X))
 
 # creates new figure for spectrogram from the stft matrix
 plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-plt.colorbar()
-plt.show()
+librosa.display.specshow(Xdb, sr=sr)
+# librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+# plt.colorbar()
+plt.savefig('/Users/cecilieneckelmann/Documents/ResearchProject/SinGan/Input/Images/{0}.png'.format(nameOfSound), bbox_inches='tight')
+# plt.show()
 
 
 # recovered_audio_orig = invert_pretty_spectrogram(wav_spectrogram, fft_size = fft_size,
